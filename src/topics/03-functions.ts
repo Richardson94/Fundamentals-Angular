209e248a5d1d442b1acb83ca68fdc62048583ba2
Richardson94 <richcori94@gmail.com>
2023-05-19 23:47:21 -0400

    angular class 05

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -14,10 +14,31 @@ function multiply(
   return firstNumber * base;
 }
 
-const result: number = addNumbers(1, 2);
-const result2: string = addNubmersArrow(1, 2);
-const multiplyResult: string = multiply(5);
+// const result: number = addNumbers(1, 2);
+// const result2: string = addNubmersArrow(1, 2);
+// const multiplyResult: number = multiply(5);
 
-console.log({ result, result2, multiplyResult });
+// console.log({ result, result2, multiplyResult });
+
+interface Character {
+  name: string;
+  hp: number;
+  showHP: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+
+const strider: Character = {
+  name: 'Aragon',
+  hp: 50,
+  showHP() {
+    console.log(`healt Points ${this.hp}`);
+  },
+};
+
+healCharacter(strider, 30);
+strider.showHP();
 
 export {};
