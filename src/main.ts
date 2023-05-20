89ad0efa4899aa7a02ec02acd83d7f9bfbc03c56
Richardson94 <richcori94@gmail.com>
2023-05-19 23:20:18 -0400

    angular class 03

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.ts';
 //import './topics/01-basic-types';
+//import './topics/02-object-interface';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
