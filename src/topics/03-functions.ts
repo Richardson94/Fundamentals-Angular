b51c8b0bb9b9e14bb91232437fc72d5e22a9ffbb
Richardson94 <richcori94@gmail.com>
2023-05-19 23:37:38 -0400

    angular class 04

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -6,9 +6,18 @@ const addNubmersArrow = (a: number, b: number): string => {
   return `${a + b}`;
 };
 
+function multiply(
+  firstNumber: number,
+  secondNumber?: number,
+  base: number = 2
+) {
+  return firstNumber * base;
+}
+
 const result: number = addNumbers(1, 2);
 const result2: string = addNubmersArrow(1, 2);
+const multiplyResult: string = multiply(5);
 
-console.log({ result, result2 });
+console.log({ result, result2, multiplyResult });
 
 export {};
