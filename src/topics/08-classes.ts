0f15e1a58c43d528c6dcdcde88fd1b88c810fd41
Richardson94 <richcori94@gmail.com>
2023-05-22 20:35:58 -0400

    angular class 13

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -3,13 +3,36 @@ export class Person {
   //private address: string;
 
   constructor(
-    public name: string | undefined,
+    public firstName: string,
+    public lastName: string,
     private address: string = 'No Address'
+  ) {}
+}
+
+// export class Hero extends Person {
+//   constructor(
+//     public alterEgo: string,
+//     public age: number,
+//     public realName: string
+//   ) {
+//     super(realName);
+//   }
+// }
+
+export class Hero {
+  // public person: Person;
+
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string,
+    public person: Person
   ) {
-    this.name = name;
-    this.address = address;
+    //this.person = new Person(realName);
   }
 }
 
-const ironman = new Person('Ironman');
-console.log({ ironman });
+const tony = new Person('Tony', ' Stark', 'New York');
+
+const ironman = new Hero('Ironman', 45, 'Tony', tony);
+console.log(ironman);
