1784895c041052023d4cd7537dae5f5222c24979
Richardson94 <richcori94@gmail.com>
2023-05-24 22:13:01 -0400

    angular class 19

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'My first app of angular';
   public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  reset(): void {
+    this.counter = 10;
+  }
 }
