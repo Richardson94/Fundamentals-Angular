e501aa04caf37e79017402a290a0bd9155834896
Richardson94 <richcori94@gmail.com>
2023-05-27 20:51:45 -0400

    angular class 44

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
     <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs..." />
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
   `,
 })
-export class searchBoxComponent {}
+export class searchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
+}
