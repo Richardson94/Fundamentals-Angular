64b8db787eede838123ef92e177306c0bbc2b40e
Richardson94 <richcori94@gmail.com>
2023-05-27 21:24:54 -0400

    angular class 45

--- 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private gifService: GifsService) {
+    console.log(this.gifService.tagsHistory);
+  }
+
+  get tags() {
+    return this.gifService.tagsHistory;
+  }
+}
