008e22b600b8bb4a19da115f2af6cd81cc89f1eb
Richardson94 <richcori94@gmail.com>
2023-06-03 01:22:00 -0400

    angular class 52

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { searchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { GifCardComponent } from './components/gif-card/gif-card.component';
 
 @NgModule({
-  declarations: [HomePageComponent, searchBoxComponent, CardListComponent],
+  declarations: [HomePageComponent, searchBoxComponent, CardListComponent, GifCardComponent],
   exports: [HomePageComponent, searchBoxComponent],
   imports: [CommonModule],
 })
