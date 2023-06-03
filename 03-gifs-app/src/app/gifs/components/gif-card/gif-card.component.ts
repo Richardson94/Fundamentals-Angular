86e1a839872545a2d242b90e82750d10e0fa7082
Richardson94 <richcori94@gmail.com>
2023-06-03 01:36:50 -0400

    angular class 53

--- 03-gifs-app/src/app/gifs/components/gif-card/gif-card.component.ts
+++ 03-gifs-app/src/app/gifs/components/gif-card/gif-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
-  selector: 'app-gif-card',
+  selector: 'gif-card',
   templateUrl: './gif-card.component.html',
-  styleUrls: ['./gif-card.component.css']
 })
-export class GifCardComponent {
+export class GifCardComponent implements OnInit {
+  @Input()
+  public gif!: Gif;
 
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('Gif property is required');
+  }
 }
