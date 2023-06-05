aa9f8fc035d8c595b528f85f63893ce89fb7506e
Richardson94 <richcori94@gmail.com>
2023-06-05 11:46:35 -0400

    angular class 54

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { searchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifCardComponent } from './components/gif-card/gif-card.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { searchBoxComponent } from './components/search-box/search-box.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HomePageComponent, searchBoxComponent, CardListComponent, GifCardComponent],
+  declarations: [
+    HomePageComponent,
+    searchBoxComponent,
+    CardListComponent,
+    GifCardComponent,
+  ],
   exports: [HomePageComponent, searchBoxComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
 })
 export class GifsModule {}
---
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+})
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: String;
+
+  @Input()
+  public alt: String = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL property ys required');
+  }
+
+  OnLoad() {
+    console.log('Image Loaded');
+    this.hasLoaded = true;
+  }
+}
