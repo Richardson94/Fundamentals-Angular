301a1df120a92346c148e6e45d8abb688ecc5fec
Richardson94 <richcori94@gmail.com>
2023-06-07 21:53:14 -0400

    angular class 67

--- 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
   styles: [],
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
+    private router: Router,
     private countriesService: CountriesService
   ) {}
 
@@ -22,7 +26,11 @@ export class CountryPageComponent implements OnInit {
         )
       )
       .subscribe((country) => {
-        console.log({ country });
+        if (!country) {
+          return this.router.navigateByUrl('');
+        }
+        this.country = country;
+        return;
       });
   }
 }
