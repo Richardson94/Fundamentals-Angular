f6704927a7a029332658ed0c8a3cfe49d602da9a
Richardson94 <richcori94@gmail.com>
2023-06-15 23:08:27 -0400

    angular class 83

--- 05-pipesApp/src/app/products/pages/numbers-pages/numbers-pages.component.ts
+++ 05-pipesApp/src/app/products/pages/numbers-pages/numbers-pages.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-pages',
   templateUrl: './numbers-pages.component.html',
-  styleUrls: ['./numbers-pages.component.css']
+  styleUrls: ['./numbers-pages.component.css'],
 })
 export class NumbersPagesComponent {
-
+  public totalSells: number = 2567789.5567;
+  public percentage: number = 0.4856;
 }
