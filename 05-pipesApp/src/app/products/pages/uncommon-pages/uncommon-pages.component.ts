45b639db925a9ac627dce61e87a52da2d41a424d
Richardson94 <richcori94@gmail.com>
2023-06-16 21:32:53 -0400

    angular class 84

--- 05-pipesApp/src/app/products/pages/uncommon-pages/uncommon-pages.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-pages/uncommon-pages.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-pages',
   templateUrl: './uncommon-pages.component.html',
-  styleUrls: ['./uncommon-pages.component.css']
+  styleUrls: ['./uncommon-pages.component.css'],
 })
 export class UncommonPagesComponent {
+  //i18n Select
+  public name: string = 'Richardson';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'mr',
+    female: 'mrs',
+  };
 
+  changeClient(): void {
+    if (this.name === 'Richardson') {
+      this.name = 'Fernanda';
+      this.gender = 'female';
+    } else {
+      this.name = 'Richardson';
+      this.gender = 'male';
+    }
+  }
 }
