dded0f95868506b29d0ef1043c15e23d8797e087
Richardson94 <richcori94@gmail.com>
2023-06-26 17:35:52 -0400

    angular class 108

--- 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SearchPageComponent {
+  public searchInput = new FormControl('');
 
+  constructor(private heroeService: HeroesService) {}
 }
